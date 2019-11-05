78192df8ce415562987d90a43655596127181be4
Murray Fordyce <unDeadHerbs@gmail.com>
2019-11-05 09:23:51 -0600

    Found a solution
    https://stackoverflow.com/q/58705210/4499969

--- VariadicTemplateDelegiation.cpp
+++ VariadicTemplateDelegiation.cpp
@@ -9,21 +9,19 @@ struct Container{
   Container(int v):value(v){}
 };
 
-int f(Container<typeA> param){
+void f(Container<typeA> param){
   std::cout<<"Got typeA with value "<<param.value<<std::endl;
-  return param.value;
 }
-int f(Container<typeB> param){
+void f(Container<typeB> param){
   std::cout<<"Got typeB with value "<<param.value<<std::endl;
-  return param.value;
 }
 
 template <typename...T>
 void f(T... params){
   // f(params);...
   // f(params)...;    // Fail to compile
-  // {f(params)...};
-  std::vector<int> v={f(params)...}; // Works
+  (f(params),...);
+   //std::vector<int> v={f(params)...}; // Works
 }
 
 
@@ -36,7 +34,7 @@ void g(T param,R...rest){
 
 int main(){
   auto a=Container<typeA>(5);
-  auto b=Container<typeA>(10);
+  auto b=Container<typeB>(10);
   f(a,b);
   g(a,b);
   return 0;
